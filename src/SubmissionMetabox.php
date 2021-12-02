4537b632dfcc275f8bc8f61621b586f8432d8323
Gautam Garg <admin@knitpay.org>
2021-12-02 17:53:22 +0530

    Added feature of Submission Meta Box in React. Update SubmissionMetabox.php and SubmissionMetaboxReact.php

--- src/SubmissionMetabox.php
+++ src/SubmissionMetabox.php
@@ -10,6 +10,8 @@ use NF_Abstracts_SubmissionMetabox;
 final class SubmissionMetabox extends NF_Abstracts_SubmissionMetabox {
 
 	public function __construct() {
+		$this->registerReactMetabox();
+		
 		parent::__construct();
 
 		$this->_title = __( 'Knit Pay Payment Details', 'ninja-forms' );
@@ -57,5 +59,29 @@ final class SubmissionMetabox extends NF_Abstracts_SubmissionMetabox {
 			echo '</dd>';
 		}
 
+	}
+	
+	/**
+	 * Register the React metabox for calculations
+	 *
+	 * @return void
+	 */
+	protected function registerReactMetabox(): void {
+		add_filter( 'nf_react_table_extra_value_keys', array( $this, 'nfAddMetabox' ) );
+	}
+	
+	
+	/**
+	 * Add a metabox constructor to the react.js submissions page
+	 *
+	 * @param array $metaboxHandlers
+	 * @return array
+	 */
+	public function nfAddMetabox( array $metaboxHandlers ): array {
+		require_once 'SubmissionMetaboxReact.php';
+		
+		$metaboxHandlers['knit_pay_payment_details'] = 'Pronamic\WordPress\Pay\Extensions\NinjaForms\SubmissionMetaboxReact';
+		
+		return $metaboxHandlers;
 	}
 }
